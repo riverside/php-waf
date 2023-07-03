51420f03610d9f09232a95ca54905074e2fd69d4
Dimitar Ivanov <biggie@abv.bg>
2023-07-03 17:23:28 +0300

    Added CRLF filter

---
+++ src/Filter/CRLF.php
@@ -0,0 +1,43 @@
+<?php
+namespace PhpWaf\Filter;
+
+use PhpWaf\BaseFilter;
+
+/**
+ * Class CRLF
+ *
+ * @package PhpWaf\Filter
+ */
+class CRLF extends BaseFilter
+{
+    /**
+     * @var string
+     */
+    protected $payloads_file = "crlf.txt";
+
+    /**
+     * Check given string
+     *
+     * @param string $value
+     * @return bool
+     */
+    public function safe(string $value): bool
+    {
+        foreach ($this->payloads as $payload)
+        {
+            $payload = trim($payload);
+
+            if (empty($payload) || strpos($payload, '#') === 0)
+            {
+                continue;
+            }
+
+            if ($payload == $value || stripos($value, $payload) !== false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
