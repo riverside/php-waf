21076c2d4f9b954d570710c4df18008d5c0add2b
Dimitar Ivanov <biggie@abv.bg>
2024-09-06 21:07:02 +0300

    Update Firewall.php

--- src/Firewall.php
+++ src/Firewall.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace PhpWaf;
 
 /**
@@ -9,23 +11,43 @@ namespace PhpWaf;
 class Firewall
 {
     /**
+     * @var int
+     */
+    const MODE_LOG_AND_BLOCK = 1;
+
+    /**
+     * @var int
+     */
+    const MODE_BLOCK = 2;
+
+    /**
+     * @var int
+     */
+    const MODE_LOG = 3;
+
+    /**
+     * @var string
+     */
+    const COMMON_LOG_FORMAT = '%m (%i) [%f] - %v';
+
+    /**
      * Available filters and their default status
      *
      * @var array
      */
-    protected $filters = array(
+    protected $filters = [
         'SQL' => true,
         'XML' => false,
         'XSS' => true,
         'CRLF' => true,
-    );
+    ];
 
     /**
      * The mode in that firewall is running
      *
      * @var int Valid modes are: 1 - block & log; 2 - block only; 3 - log only
      */
-    protected $mode = 1;
+    protected $mode = self::MODE_LOG_AND_BLOCK;
 
     /**
      * The log file path
@@ -47,15 +69,15 @@ class Firewall
      *
      * @var string
      */
-    protected $log_format = '%m (%i) [%f] - %v';
+    protected $log_format = self::COMMON_LOG_FORMAT;
 
     /**
      * Firewall constructor.
      *
      * @param int $mode
-     * @throws \Exception
+     * @throws Exception
      */
-    public function __construct(int $mode=1)
+    public function __construct(int $mode=self::MODE_LOG_AND_BLOCK)
     {
         $this->setMode($mode);
     }
@@ -65,7 +87,7 @@ class Firewall
      */
     public static function block()
     {
-        header("HTTP/1.1 400 Bad Request");
+        http_response_code(400);
         exit;
     }
 
@@ -75,24 +97,24 @@ class Firewall
      * @param string $value
      * @param string $filter
      * @return Firewall
-     * @throws \Exception
+     * @throws Exception
      */
     public function log(string $value, string $filter): Firewall
     {
         if (empty($this->log_file))
         {
-            throw new \Exception("Empty log_file.");
+            throw new Exception(Exception::ERROR_EMPTY_LOG_FILE, Exception::ERROR_EMPTY_LOG_FILE_CODE);
         }
 
         if (empty($this->log_format))
         {
-            throw new \Exception("Empty log_format.");
+            throw new Exception(Exception::ERROR_EMPTY_LOG_FORMAT, Exception::ERROR_EMPTY_LOG_FORMAT_CODE);
         }
 
         $data = str_replace(
-            array('%f', '%v', '%i', '%d', '%t', '%m', '%u'),
-            array($filter, $value, $_SERVER['REMOTE_ADDR'], date('Y-m-d'), date('H:i:s'),
-                date('Y-m-d H:i:s'), time()),
+            ['%f', '%v', '%i', '%d', '%t', '%m', '%u'],
+            [$filter, $value, $_SERVER['REMOTE_ADDR'], date('Y-m-d'), date('H:i:s'),
+                date('Y-m-d H:i:s'), time()],
             $this->log_format);
 
         file_put_contents($this->log_file, "\nwarn $data", FILE_APPEND);
@@ -131,13 +153,13 @@ class Firewall
      *
      * @param int $value
      * @return Firewall
-     * @throws \Exception
+     * @throws Exception
      */
     public function setMode(int $value): Firewall
     {
-        if (!in_array($value, range(1,3)))
+        if (!in_array($value, [self::MODE_LOG_AND_BLOCK, self::MODE_BLOCK, self::MODE_LOG]))
         {
-            throw new \Exception("Unknown mode {$value}.");
+            throw new Exception(Exception::ERROR_UNKNOWN_MODE, Exception::ERROR_UNKNOWN_MODE_CODE);
         }
         $this->mode = $value;
 
@@ -149,13 +171,13 @@ class Firewall
      *
      * @param string $filter
      * @return Firewall
-     * @throws \Exception
+     * @throws Exception
      */
     public function enable(string $filter): Firewall
     {
         if (!array_key_exists($filter, $this->filters))
         {
-            throw new \Exception("Unknown filter {$filter}.");
+            throw new Exception(Exception::ERROR_UNKNOWN_FILTER, Exception::ERROR_UNKNOWN_FILTER_CODE);
         }
         $this->filters[$filter] = true;
 
@@ -167,13 +189,13 @@ class Firewall
      *
      * @param string $filter
      * @return Firewall
-     * @throws \Exception
+     * @throws Exception
      */
     public function disable(string $filter): Firewall
     {
         if (!array_key_exists($filter, $this->filters))
         {
-            throw new \Exception("Unknown filter {$filter}.");
+            throw new Exception(Exception::ERROR_UNKNOWN_FILTER_CODE, Exception::ERROR_UNKNOWN_FILTER_CODE);
         }
         $this->filters[$filter] = false;
 
@@ -186,24 +208,24 @@ class Firewall
      * @param string $value
      * @param string $filter
      * @return Firewall
-     * @throws \Exception
+     * @throws Exception
      */
     public function handle(string $value, string $filter): Firewall
     {
-        if ($this->mode == 1)
+        if ($this->mode == self::MODE_LOG_AND_BLOCK)
         {
             // log & block
             $this->log($value, $filter);
             self::block();
         }
 
-        if ($this->mode == 2)
+        if ($this->mode == self::MODE_BLOCK)
         {
             // block only
             self::block();
         }
 
-        if ($this->mode == 3)
+        if ($this->mode == self::MODE_LOG)
         {
             // log only
             $this->log($value, $filter);
@@ -217,13 +239,13 @@ class Firewall
      *
      * @param string $filter
      * @return BaseFilter
-     * @throws \Exception
+     * @throws Exception
      */
     public function getFilterInstance(string $filter): BaseFilter
     {
         if (!array_key_exists($filter, $this->getFilters()))
         {
-            throw new \Exception("Unknown filter {$filter}.");
+            throw new Exception(Exception::ERROR_UNKNOWN_FILTER_CODE, Exception::ERROR_UNKNOWN_FILTER_CODE);
         }
 
         $class = "PhpWaf\\Filter\\$filter";
@@ -236,7 +258,7 @@ class Firewall
      *
      * @param string $filter
      * @return Firewall
-     * @throws \Exception
+     * @throws Exception
      */
     public function runFilter(string $filter): Firewall
     {
@@ -265,7 +287,7 @@ class Firewall
      * Runs all the enabled filters
      *
      * @return Firewall
-     * @throws \Exception
+     * @throws Exception
      */
     public function run(): Firewall
     {
@@ -289,5 +311,22 @@ class Firewall
     public function getFilters(): array
     {
         return $this->filters;
+    }
+
+    /**
+     * Get filter's current state
+     *
+     * @param string $filter
+     * @return bool
+     * @throws Exception
+     */
+    public function getFilter(string $filter): bool
+    {
+        if (!array_key_exists($filter, $this->filters))
+        {
+            throw new Exception(Exception::ERROR_UNKNOWN_FILTER_CODE, Exception::ERROR_UNKNOWN_FILTER_CODE);
+        }
+
+        return $this->filters[$filter];
     }
 }
